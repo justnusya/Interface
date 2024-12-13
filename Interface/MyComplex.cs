e44b06fa90187581c19a32b5aa5d59bf0be0cb42
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-12-13 19:53:24 +0200

    Added the rest of methods and override ToString() for MyComplex class

--- Interface/MyComplex.cs
+++ Interface/MyComplex.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Interface
 {
-    class MyComplex: IMyNumber<MyFrac>, IComparable<MyFrac>
+    public class MyComplex: IMyNumber<MyFrac>
     {
         double re, im;
         public MyComplex(double re, double im)
@@ -30,6 +30,25 @@ namespace Interface
         public MyComplex Add(MyComplex that)
         {
             return new MyComplex(this.re + that.re, this.im + that.im);
+        }
+        public MyComplex Substruct(MyComplex that)
+        {
+            return new MyComplex(this.re - that.re, this.im - that.im);
+        }
+        public MyComplex Multiply(MyComplex that)
+        {
+            return new MyComplex(this.re * that.re - this.im*that.im, this.re * that.im + this.im *that.re);
+        }
+        public MyComplex Divide(MyComplex that)
+        {
+            double denomin = that.re * that.re + that.im * that.im;
+            double real = (this.re * that.re + this.im * that.im) / denomin;
+            double imag = (this.im * that.re - this.re * that.im) / denomin;
+            return new MyComplex(real, imag);
+        }
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
         }
     }
 }
