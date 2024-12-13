6fef5baaed2525240e890e4d3328887a9fc806e1
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-12-13 19:37:59 +0200

    Added MyComplex class with a usual constructor and the string one (along with exception). Added a method "Add"

---
+++ Interface/MyComplex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class MyComplex: IMyNumber<MyFrac>, IComparable<MyFrac>
+    {
+        double re, im;
+        public MyComplex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+        public MyComplex(string complex)
+        {
+            try
+            {
+                string[] data = complex.Trim().Split('+');
+                re = double.Parse(data[0]);
+                im = double.Parse(data[1].Replace("i", "").Trim());
+            }
+            catch
+            {
+                throw new ArgumentException("Wrong input");
+            }
+        }
+        public MyComplex Add(MyComplex that)
+        {
+            return new MyComplex(this.re + that.re, this.im + that.im);
+        }
+    }
+}
